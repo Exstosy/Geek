ecc62cff4ea46d7320f6fe4c92e7fc4e4809024a
Exstosy <47812846+Exstosy@users.noreply.github.com>
2022-12-15 11:54:18 +0200

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число. Выхода из программы команда 0.");
+ while(true)
+ {
+  string number = Console.ReadLine();
+   if (!number.Equals("0"))
+      Console.WriteLine("{0}->{1}" , number , number[1] );
+   else
+   break;
+   }
+ 
+
+
+  
