3c95fa84d23459fa533ae63234be066fdbebfa37
Exstosy <47812846+Exstosy@users.noreply.github.com>
2022-12-15 11:55:43 +0200

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите число от 1-7");
+ int day = Convert.ToInt32(Console.ReadLine());
+  if(day < 5 && day < 7 ){
+  Console.WriteLine("нет");
+  }
+else if (day >= 6 && day <= 7){
+    Console.WriteLine("да");
+}
